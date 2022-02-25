b9a5bab10a3a590a56e469924d39e897bcb25623
bansa2 <ayushbansal506@gmail.com>
2022-02-26 00:55:15 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,18 +1,18 @@
 resource "aws_internet_gateway" "main-igw" {
-    vpc_id = "${aws_vpc.main.id}"
+    vpc_id = aws_vpc.main.id
     tags = {
         Name = "main-igw"
     }
 }
 
 resource "aws_route_table" "public-subnet-rt" {
-    vpc_id = "${aws_vpc.main.id}"
+    vpc_id = aws_vpc.main.id
     
     route {
         //associated subnet can reach everywhere
         cidr_block = "0.0.0.0/0" 
         //Route table uses this IGW to reach internet
-        gateway_id = "${aws_internet_gateway.main-igw.id}" 
+        gateway_id = aws_internet_gateway.main-igw.id 
     }
     
     tags = {
@@ -21,13 +21,13 @@ resource "aws_route_table" "public-subnet-rt" {
 }
 
 resource "aws_route_table_association" "RT-public-subnet-1-associate"{
-    subnet_id = "${aws_subnet.public-subnet-1.id}"
-    route_table_id = "${aws_route_table.public-subnet-rt.id}"
+    subnet_id = aws_subnet.public-subnet-1.id
+    route_table_id = aws_route_table.public-subnet-rt.id
 }
 
 resource "aws_route_table_association" "RT-public-subnet-2-associate"{
-    subnet_id = "${aws_subnet.public-subnet-2.id}"
-    route_table_id = "${aws_route_table.public-subnet-rt.id}"
+    subnet_id = aws_subnet.public-subnet-2.id
+    route_table_id = aws_route_table.public-subnet-rt.id
 }
 
 
