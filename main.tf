2f243d9f7ccdd9845d77966733533b3957029cf3
bansa2 <ayushbansal506@gmail.com>
2022-02-26 00:54:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
 
 
 resource "aws_subnet" "public-subnet-1" {
-    vpc_id = "${aws_vpc.main.id}"
+    vpc_id = aws_vpc.main.id
     cidr_block = "100.10.10.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = var.availability_zones[0]
@@ -18,7 +18,7 @@ resource "aws_subnet" "public-subnet-1" {
 }
 
 resource "aws_subnet" "public-subnet-2" {
-    vpc_id = "${aws_vpc.main.id}"
+    vpc_id = aws_vpc.main.id
     cidr_block = "100.10.20.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = var.availability_zones[1]
