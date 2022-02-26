8e8b48f0d809629421524b0afd7b235c2f6cebf9
bansa2 <ayushbansal506@gmail.com>
2022-02-26 13:07:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws"{
     region = "ap-south-1"
-    access_key = "AKIAXXB3S6P5HUK2IWFO"
-    secret_key = "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"
+    access_key = "AKIAXXB3S6P5FWKKY4FB"
+    secret_key = "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"
 }
 
 
